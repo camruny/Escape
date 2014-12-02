f40e76f77f7278a7c95fd0e9fd788f4bf0c7f5ec
Cameron Runyeon <Cameron@client-75-102-86-107.mobility-up.psu.edu>
2014-12-02 13:34:17 -0500

    Fixed Blinking Mario

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -20,7 +20,7 @@ public class StartPanel extends JPanel implements ActionListener, KeyListener{
         theme1 = new JButton("Halloween Theme");
         theme2 = new JButton("Thanksgiving Theme");
         theme3 = new JButton("Christmas Theme");
-        chosenTheme = new Theme(new ImageIcon("images/mario/mario1-copy.png"), new ImageIcon("images/bowser/bowser.png"), new ImageIcon("images/backgrounds/bg-thanksgiving.png"));
+        chosenTheme = new Theme(new ImageIcon("images/mario/mario1.png"), new ImageIcon("images/bowser/bowser.png"), new ImageIcon("images/backgrounds/bg-thanksgiving.png"));
         gameBoard = new GamePanel(chosenTheme);
         chosenTheme.backgroundPic = chosenTheme.backgroundIcon.getImage();
 
@@ -110,7 +110,7 @@ public class StartPanel extends JPanel implements ActionListener, KeyListener{
             gameBoard.changeCharacterLeft();
         }
         if(obj == theme1){
-            gameBoard.mario1 = new ImageIcon("images/mario/");
+            gameBoard.mario1 = new ImageIcon("images/mario/mario1.png");
             chosenTheme.playerPic = new ImageIcon("images/mario/mario1.png");
             chosenTheme.enemyPic = new ImageIcon("images/goomba/goomba_LF.png");
             chosenTheme.backgroundIcon = new ImageIcon("images/backgrounds/halloween.png");
