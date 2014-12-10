c6a9c9b1a2d4f016768c8fc8f29c28378b5afd0d
Shane Eckenrode <sue2@psu.edu>
2014-12-10 17:49:04 -0500

    Added mac display support

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,12 +2,22 @@
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.UIManager;
 
 public class GameFrame extends JFrame {
     StartPanel sp;
     
     public GameFrame()   {
         super("Escape");
+        
+        //Fixes issues with mac displaying background colors.
+        try 
+        {
+            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+        }catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
         
         sp = new StartPanel();
         getContentPane().setLayout(new BorderLayout());
